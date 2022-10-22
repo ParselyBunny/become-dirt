ef42b20c5679dfd93facc9a034fec689cf1c2cb9
ParselyBunny <gunininanrunin@gmail.com>
2022-10-22 13:47:33 -0700

    WIP NPC interactable

---
+++ Assets/Scripts/Interactables/NPC.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach this to an object with an animator
+/// that has a trigger to allow player
+/// to toggle it. Intended for openable
+/// objects like doors and drawers.
+/// </summary>
+public class NPC : Interactable
+{
+    [Tooltip("Name of the NPC to be displayed on the screen.")] public string Name = "Unknown";
+    [Tooltip("The knot to jump to in the Ink Story.")] public string InkKnot = "Intro";
+    private Animator _animator;
+    
+    void Start() {
+        _animator = (GetComponent<Animator>() != null) ? GetComponent<Animator>() : new Animator();
+    }
+
+    protected override void Interact()
+    {
+        base.Interact();
+
+        //_animator.SetTrigger(TriggerName);
+    }
+}
