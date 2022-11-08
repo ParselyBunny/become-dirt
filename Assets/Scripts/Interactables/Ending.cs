f6d9298e041eb96ac9db8a5136738bf20afee3b0
ParselyBunny <gunininanrunin@gmail.com>
2022-11-07 20:41:18 -0800

    Add ending

---
+++ Assets/Scripts/Interactables/Ending.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using JTools;
+
+/// <summary>
+/// Attach this to the object that
+/// will end the game.
+/// </summary>
+public class Ending : Interactable
+{
+    public AudioClip EndingMusic;
+    private float _musicVolume;
+    public override void Interact()
+    {
+        base.Interact();
+        base.SetAllowInteractSound(false);
+
+        ImpactController.current.inputComponent.lockInput = true;
+        _musicVolume = AudioManager.GetVolumeNormalized(AudioManager.MixerLabel.Music);
+        AudioManager.SetVolume(AudioManager.MixerLabel.Music, -80.0f);
+        UIMenus.SetActiveMenu("Black Screen");
+
+        StartCoroutine(End());
+    }
+
+    private IEnumerator End() {
+        yield return new WaitForSecondsRealtime(3);
+
+        AudioManager.PlayMusic(EndingMusic);
+        AudioManager.SetVolume(AudioManager.MixerLabel.Music, _musicVolume);
+        UIMenus.SetActiveMenu("Ending");
+
+        StartCoroutine(Quit());
+    }
+
+    private IEnumerator Quit() {
+        yield return new WaitForSecondsRealtime(13);
+
+        UIUtils.QuitGame();
+    }
+}
