ef571b104bcb93d527c4081b8eb148c5e221eb0d
Jane P <badidea2004@gmail.com>
2023-10-04 14:53:40 -0500

    Basic Font substitution

---
+++ Assets/FontSwitcher.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FontSwitcher : MonoBehaviour
+{
+    public List<TextMeshProUGUI> TextItemsBig;
+    public List<TextMeshProUGUI> TextItemsDefault;
+    
+    public TMP_FontAsset Fancy_Big;
+
+    public TMP_FontAsset Fancy_Default;
+
+    public TMP_FontAsset Legible_Big;
+
+    public TMP_FontAsset Legible_Default;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (TextMeshProUGUI text in Resources.FindObjectsOfTypeAll(typeof(TextMeshProUGUI)))
+        {
+            if (text.font == Fancy_Big || text.font == Legible_Big)
+            {
+                TextItemsBig.Add(text);
+            }
+
+            if (text.font == Fancy_Default || text.font == Legible_Default)
+            {
+                TextItemsDefault.Add(text);
+            }
+        }
+
+    }
+
+    public void switchStyle(Boolean fancy)
+    {
+        if (fancy)
+        {
+            foreach (TextMeshProUGUI big in TextItemsBig)
+            {
+                big.font = Fancy_Big;
+            }
+
+            foreach (TextMeshProUGUI def in TextItemsDefault)
+            {
+                def.font = Fancy_Default;
+            }
+        }
+        else
+        {
+            foreach (TextMeshProUGUI big in TextItemsBig)
+            {
+                big.font = Legible_Big;
+            }
+
+            foreach (TextMeshProUGUI def in TextItemsDefault)
+            {
+                def.font = Legible_Default;
+            }
+        }
+    }
+}
